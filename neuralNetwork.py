ec48ea8c1d7e21fa3360add66cac7d703333887c
Conan Chen <cchen163@jhu.edu>
2018-11-03 14:01:05 -0400

    Generalizing to multiple potential outputs.
    Extended the code to account for multiple output situations. Refactored
    backpropagation to use matrix multiplication.

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -5,102 +5,128 @@ import numpy as np
 import pandas as pd
 import math
 from scipy.special import \
-	expit  # Fastest per https://stackoverflow.com/questions/43024745/applying-a-function-along-a-numpy-array
-
-# Importing the excel data for the XOR problem.
-df = pd.read_excel('XOR_dataset.xlsx', 'Sheet1')  # Pandas dataframe
-
-# Numpy array - https://stackoverflow.com/questions/13187778/convert-pandas-dataframe-to-numpy-array-preserving-index
-data = df.values
+    expit  # Fastest per https://stackoverflow.com/questions/43024745/applying-a-function-along-a-numpy-array
+#from keras.models import Sequential
+#from keras.layers import Dense, Activation
 
 
 # Writing out the Neural Network class
 class NeuralNetwork:
-	# Constructor function
-	def __init__(self, numinputs, numhidden, numoutputs):
-		self.numInputs = numinputs
-		self.numHidden = numhidden
-		self.numOutputs = numoutputs
-		self.weightsIH = np.zeros(shape=(numhidden, numinputs))
-		self.weightsHO = np.zeros(shape=(numoutputs, numhidden))
-		self.biasH = np.zeros(shape=(numhidden, 1))
-		self.biasO = np.zeros(shape=(numoutputs, 1))
+    # Constructor function
+    def __init__(self, num_inputs, num_hidden, num_outputs):
+        # self.numInputs = num_inputs
+        # self.numHidden = num_hidden
+        # self.numOutputs = num_outputs
+        self.weightsIH = np.random.random((num_hidden, num_inputs)) - 0.5
+        self.weightsHO = np.random.random((num_outputs, num_hidden)) - 0.5
+        self.biasH = np.random.random((num_hidden, 1)) - 0.5
+        self.biasO = np.random.random((num_outputs, 1)) - 0.5
 
-	def train(self, dataSet, epochs=5, eta=0.5):
-		ansSet = dataSet[:, 0]  # first column
-		trnSet = dataSet[:, 1:]
-		sumSqErr = 0
+        #self.weightsIH = np.zeros(shape=(num_hidden, num_inputs))
+        #self.weightsHO = np.zeros(shape=(num_outputs, num_hidden))
+        #self.biasH = np.zeros(shape=(num_hidden, 1))
+        #self.biasO = np.zeros(shape=(num_outputs, 1))
 
-		for epoch in range(epochs):
+    def train(self, ans_set, inp_set, epochs=5, eta=0.5):
 
-			print('Epoch number: ', epoch)
-			sumSqErr = 0
-			iterations = range(trnSet.shape[0])
-			for i in iterations:
-				x = trnSet[i, :]
-				x = x.reshape(x.shape[0], 1)
-				y = ansSet[i]
-				z_hid, a_hid, z_out, y_hat = self.feedforward(x)
-				sumSqErr = sumSqErr + (y - y_hat) ** 2
-				temp_weights_ho = self.weightsHO + eta * (y - y_hat) * np.multiply(expit(z_out), 1-expit(z_out)) * np.transpose(a_hid)
-				temp_weights_ih = self.weightsIH + eta * (y - y_hat) * np.multiply(expit(z_out), 1-expit(z_out)) * np.transpose(self.weightsHO) * np.multiply(expit(z_hid), 1-expit(z_hid)) * x
-				temp_bias_o = self.biasO + eta * (y - y_hat) * np.multiply(expit(z_out), 1-expit(z_out))
-				temp_bias_h = self.biasH + eta * (y - y_hat) * np.multiply(expit(z_out), 1-expit(z_out)) * np.multiply(np.transpose(self.weightsHO), np.multiply(expit(z_hid), 1-expit(z_hid)))
+        for epoch in range(epochs):
 
-				# Fourth, updating everything.
-				self.weightsHO = temp_weights_ho
-				self.weightsIH = temp_weights_ih
-				self.biasO = temp_bias_o
-				self.biasH = temp_bias_h
-			print('Sum Squared Error:', np.asscalar(sumSqErr))
+            print('Epoch number: ', epoch)
+            sumSqErr = 0
+            iterations = range(inp_set.shape[0])
+            for i in iterations:
+                x = inp_set[i, :]
+                x = x.reshape(x.shape[0], 1)  # reshape as column vector
+                y = ans_set[i, :]
+                y = y.reshape(y.shape[0], 1)  # reshape as column vector
+                z_hid, a_hid, z_out, y_hat = self.feedforward(x)
+                sumSqErr = sumSqErr + (y - y_hat) ** 2
 
-			if sumSqErr < 0.001:
-				break
+                delta = np.multiply((y - y_hat), NeuralNetwork.sigmoid(z_out, True))  # this is (y-y_hat)*o'(z_out)
+                temp_weights_ho = self.weightsHO + eta * np.matmul(delta, np.transpose(a_hid))
+                temp_weights_ih = self.weightsIH + eta * np.matmul(
+                    np.multiply(np.matmul(np.transpose(self.weightsHO), delta), NeuralNetwork.sigmoid(z_hid, True)),
+                    np.transpose(x))
+                temp_bias_o = self.biasO + eta * delta
+                temp_bias_h = self.biasH + eta * np.multiply(np.matmul(np.transpose(self.weightsHO), delta),
+                                                             NeuralNetwork.sigmoid(z_hid, True))
+
+                # Fourth, updating everything.
+                self.weightsHO = temp_weights_ho
+                self.weightsIH = temp_weights_ih
+                self.biasO = temp_bias_o
+                self.biasH = temp_bias_h
+
+            print('Sum Squared Error:', np.asscalar(sumSqErr))
+
+            if sumSqErr < 0.001:
+                break
+
+    def validate(self, dataSet):
+        ansSet = dataSet[:, 0]  # first column
+        valSet = dataSet[:, 1:]
+
+        totalCorrect = 0
+        totalTested = valSet.shape[0]
+        iterations = range(valSet.shape[0])
+
+        for i in iterations:
+            x = valSet[i, :]
+            x = x.reshape(x.shape[0], 1)  # Reformat as column vector
+            y = ansSet[i]
+            z_hid, a_hid, z_out, y_hat = self.feedforward(x)
+            if round(np.asscalar(y_hat)) == y:
+                totalCorrect += 1
+
+        return totalCorrect / totalTested
+
+    def feedforward(self, x):
+        z_hid = np.matmul(self.weightsIH, x) + self.biasH
+        a_hid = expit(z_hid)
+        z_out = np.matmul(self.weightsHO, a_hid) + self.biasO
+        y_hat = expit(z_out)
+        return z_hid, a_hid, z_out, y_hat
+
+    @staticmethod
+    def sigmoid(x, derivative=False):
+        if derivative:
+            return np.multiply(expit(x), (1 - expit(x)))
+        else:
+            return expit(x)
 
 
-	def validate(self,dataSet):
-		ansSet = dataSet[:, 0]  # first column
-		valSet = dataSet[:, 1:]
+# Importing the excel data for the XOR problem. Pandas dataframe.
+df = pd.read_excel('XOR_dataset.xlsx', 'Sheet1')
 
-		totalCorrect = 0
-		totalTested = valSet.shape[0]
-		iterations = range(valSet.shape[0])
-
-		for i in iterations:
-			x = valSet[i, :]
-			x = x.reshape(x.shape[0], 1)
-			y = ansSet[i]
-			z_hid, a_hid, z_out, y_hat = self.feedforward(x)
-			if round(np.asscalar(y_hat)) == y:
-				totalCorrect += 1
-
-		return totalCorrect/totalTested
-
-
-	def feedforward(self, x):
-		z_hid = np.matmul(self.weightsIH, x) + self.biasH
-		a_hid = expit(z_hid)
-		z_out = np.matmul(self.weightsHO, a_hid) + self.biasO
-		y_hat = expit(z_out)
-		return z_hid, a_hid, z_out, y_hat
-
-	@staticmethod
-	def sigmoid(x):
-		return 1 / (1 + math.exp(-x))
-
-
-# Read in data for the XOR Dataset. Alternatively, we can just hard-code this in.
-
+# Numpy array - https://stackoverflow.com/questions/13187778/convert-pandas-dataframe-to-numpy-array-preserving-index
+data = df.values
+ans_set = data[:, 0:1]  # first column; Y
+inp_set = data[:, 1:]  # input matrix; X
 
 # Defining a learning rate
 eta = 0.5
 
 # Creating a neural network object.
-nn = NeuralNetwork(2, 2, 1)
+nn = NeuralNetwork(2, 4, 1)
 
 # Training the neural network.
-nn.train(data, 15000, eta)
+#nn.train(data, 15000, eta)
+nn.train(ans_set, inp_set, 15000, eta)
 
 # Validating the neural network.
 print('Success rate: ', nn.validate(data))
 
+# Let's compare this to the Keras neural network libraries. We'll learn that and also do validation on our own code at the same time.
+
+#model = Sequential([
+#    Dense(2, input_shape=(2,)),
+#    Activation('sigmoid'),
+#    Dense(1),
+#    Activation('sigmoid'),
+#])
+
+#model.compile(loss='mean_squared_error', optimizer='rmsprop')
+#y = data[:, 0]  # first column
+#X = data[:, 1:]
+#model.fit(X, y, batch_size=1, epochs=10000)
+#print(model.predict_proba(X))
